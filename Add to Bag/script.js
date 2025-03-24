a456b7ca6b927363f74b176a7ea81890431b06d5
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-03-24 23:56:03 +0530

    Add to Bag Completed

---
+++ Add to Bag/script.js
@@ -0,0 +1,27 @@
+const toast = document.querySelector('.toast');
+const cartButton = document.querySelector('.button_div button');
+const sizeButtons = document.querySelectorAll('.size_card div');
+const productNameSpan = document.querySelector('.toast span');
+
+let prevCard = null;
+sizeButtons.forEach((card) => { 
+    card.addEventListener('click', function() { 
+        if(prevCard) {
+            console.log(prevCard);
+            prevCard.classList.remove('activeCard');
+            console.log(prevCard);
+        }
+        this.classList.add('activeCard'); 
+        prevCard = this;
+    });
+});
+
+cartButton.addEventListener('click', () => {
+    toast.style.transform = `translateX(0px)`;
+    toast.style.transition = `0.3s ease-in`;
+
+    setTimeout(() => {
+        toast.style.transform = `translateX(500px)`;
+        toast.style.transition = `0.3s ease-out`;
+    }, 1000);
+});
