328b7d0f212f41182ae4f6bcdf3ce78ead7b12af
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-04-07 01:26:01 +0530

    Create Account Page UI 100% Done

--- Create Account Page/script.js
+++ Create Account Page/script.js
@@ -1,10 +1,20 @@
+const errorMessage = document.getElementById('errorMsg');
 const inputElem = document.querySelectorAll('input');
+const subBtn = document.querySelector('button');
 
-inputElem.forEach((inputs)=>{
-    inputs.addEventListener('keypress',(e)=>{
-        e.target.key === "SPACE" ?
-         console.log(e.target.key):
-         console.log(e.target.key)
+inputElem.forEach((inputs) => {
+    inputs.addEventListener('keypress', (e) => {
+        if (e.code === 'Space') {
+            errorMessage.style.visibility = 'visible';
+            subBtn.disabled = 'true';
 
-    })
-})
+            setTimeout(() => {
+                errorMessage.style.visibility = 'hidden'; d
+            }, 900);
+        } else {
+            errorMessage.style.visibility = 'hidden';
+            subBtn.disabled = 'false';
+        }
+    });
+
+});
