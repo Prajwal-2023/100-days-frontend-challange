50e3357dc905d56952d7ad6e4503cf24a40858f1
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-04-08 22:44:20 +0530

    Added border Onfocus and blur

--- Create Account Page/script.js
+++ Create Account Page/script.js
@@ -2,7 +2,22 @@ const errorMessage = document.getElementById('errorMsg');
 const inputElem = document.querySelectorAll('input');
 const subBtn = document.querySelector('button');
 
+
+
+
 inputElem.forEach((inputs) => {
+
+    inputs.addEventListener('focus',(parentElem)=>{
+        console.log(parentElem.target.offsetParent);
+        parentElem.target.offsetParent.style.border = `1px solid red`;
+    })
+
+    inputs.addEventListener('blur',(parentElem)=>{
+        console.log(parentElem.target.offsetParent);
+        parentElem.target.offsetParent.style.border = `none`;
+    })
+
+
     inputs.addEventListener('keypress', (e) => {
         if (e.code === 'Space') {
             errorMessage.style.visibility = 'visible';
